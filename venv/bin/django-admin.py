7818cc2ee1e21eb8cb4bc72775c963987ee67471
uzov <uzov@cg.ru>
2022-03-28 21:59:44 +0300

    commit

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/evgeny/PycharmProjects/djangoreactproject/venv/bin/python3
+#!/home/evgeny/djangoproject/venv/bin/python3
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
